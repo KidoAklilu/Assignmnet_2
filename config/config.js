36f644c9c2d2591f9b648030a4c67f6deb226549
KidoAklilu <Kidoget321@gmail.com>
2022-10-21 02:16:19 -0400

    ATLAS COMPLETEDD

--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
 export const Secret = 'MySecret' // encrpt session information
 
-export const MongoURI = 'mongodb://localhost/phonebook'
+// export const MongoURI = 'mongodb://localhost/phonebook'
+
+export const MongoURI =
+  'mongodb+srv://kd123:Black12345@cluster0.vwkzkut.mongodb.net/phonebook?retryWrites=true&w=majority'
