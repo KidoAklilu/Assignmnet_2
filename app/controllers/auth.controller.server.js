9848f9fe8da7843ca397f79bd1ce307a30cc89d9
KidoAklilu <Kidoget321@gmail.com>
2022-10-22 09:39:56 -0400

    COMPLETED ASSIGNMENT

--- app/controllers/auth.controller.server.js
+++ app/controllers/auth.controller.server.js
@@ -1,16 +1,15 @@
 import express from 'express'
 
-// need passport
+// import passport
 import passport from 'passport'
 
-// need to include the User Model for authentication
+// include the User Model for authentication
 import User from '../models/user.js'
 import { UserDisplayName } from '../utils/index.js'
 
 //Display Functions
 export function DisplayLoginPage(req, res, next) {
   if (!req.user) {
-    //User is not authenticated so render the login page
     return res.render('index', {
       title: 'Login',
       page: 'login',
@@ -90,7 +89,7 @@ export function ProcessLogoutPage(req, res, next) {
       res.end(err)
     }
 
-    console.log('user logged out successfully')
+    console.log('logged out successfully')
   })
 
   res.redirect('/login')
